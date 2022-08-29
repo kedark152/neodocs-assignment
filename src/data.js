6f9ba87130755ae583ba92f16757d2796f778b3b
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-08-29 23:11:27 +0530

    feat: added dummy data

---
+++ src/data.js
@@ -0,0 +1,49 @@
+export const data = [
+  {
+    parent: "Teacher 1",
+    isParentChecked: false,
+    childrens: [
+      { name: "Student 1", isChildChecked: false },
+      { name: "Student 2", isChildChecked: false },
+      { name: "Student 3", isChildChecked: false },
+      { name: "Student 4", isChildChecked: false },
+    ],
+  },
+  {
+    parent: "Teacher 2",
+    isParentChecked: false,
+    childrens: [
+      { name: "Student 5", isChildChecked: false },
+      { name: "Student 6", isChildChecked: false },
+      { name: "Student 7", isChildChecked: false },
+      { name: "Student 8", isChildChecked: false },
+    ],
+  },
+  {
+    parent: "Teacher 3",
+    isParentChecked: false,
+    childrens: [
+      { name: "Student 10", isChildChecked: false },
+      { name: "Student 11", isChildChecked: false },
+      { name: "Student 12", isChildChecked: false },
+    ],
+  },
+  {
+    parent: "Teacher 4",
+    isParentChecked: false,
+    childrens: [
+      { name: "Student 13", isChildChecked: false },
+      { name: "Student 14", isChildChecked: false },
+      { name: "Student 15", isChildChecked: false },
+    ],
+  },
+  {
+    parent: "Teacher 5",
+    isParentChecked: false,
+    childrens: [
+      { name: "Student 16", isChildChecked: false },
+      { name: "Student 17", isChildChecked: false },
+      { name: "Student 18", isChildChecked: false },
+    ],
+  },
+];
